b691faa08c1d1cb92782ccf9144c6e75daaa550d
Alessandro Marzoli <a.marzoli@ambrogio.com>
2016-12-01 17:49:19 +0100

    added * operation

---
+++ Tests/StringOperatorsTests.swift
@@ -0,0 +1,30 @@
+//
+//  StringOperatorsTests.swift
+//  Mechanica
+//
+//  Created by Alessandro Marzoli on 01/12/16.
+//  Copyright © 2016 Tinrobots. All rights reserved.
+//
+
+import XCTest
+@testable import Mechanica
+
+class StringOperatorsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+  
+  func test_multiply() {
+    XCTAssert("a"*2 == "aa")
+    XCTAssert("aA"*2 == "aAaA")
+    XCTAssert("Hello World "+"!"*2 == "Hello World !!")
+    XCTAssert("🇮🇹"*3 == "🇮🇹🇮🇹🇮🇹")
+  }
+
+    
+}
