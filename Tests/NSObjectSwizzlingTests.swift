0d0fe9ac5ec09df954e8926a82c033885bb7814d
Alessandro Marzoli <a.marzoli@ambrogio.com>
2017-03-02 15:42:24 +0100

    swizzling test without useing initialize() method (deprecated from Swift 3.1)

--- Tests/NSObjectSwizzlingTests.swift
+++ Tests/NSObjectSwizzlingTests.swift
@@ -26,46 +26,81 @@ import XCTest
 @testable import Mechanica
 
 
+//class Test : NSObject {
+//  dynamic func methodOne() -> Int { return 1 }
+//  dynamic func methodThree() -> String { return "three" }
+//}
+//
+//extension Test {
+//  
+//  override class func initialize() {
+//    /// Perform this one time only
+//    struct Inner {
+//      static let swizzling: () = {
+//        
+//        /// these 2 lines...
+//        //Test.swizzle(method: #selector(Test.methodOne), with: #selector(Test.methodTwo))
+//        //Test.swizzle(method: #selector(Test.methodThree), with: #selector(Test.methodFour))
+//        /// are the same as:
+//        Test.swizzle([
+//          ( #selector(Test.methodOne), #selector(Test.methodTwo) ),
+//          ( #selector(Test.methodThree), #selector(Test.methodFour) )
+//          ])
+//        
+//      }()
+//    }
+//    let _ = Inner.swizzling
+//  }
+//  
+//  func methodTwo() -> Int { return methodTwo() + 10 }
+//  
+//  func methodFour() -> String { return methodFour() + "!!" }
+//  
+//}
+//
+//class NSObjectSwizzlingTests: XCTestCase {
+//  
+//  func test_swizzlingExtension() {
+//    let test = Test()
+//    XCTAssertTrue(test.methodOne() == 11)
+//    XCTAssertTrue(test.methodTwo() == 1)
+//    XCTAssertTrue(test.methodThree() == "three!!")
+//    XCTAssertTrue(test.methodFour() == "three")
+//  }
+//  
+//}
+
 class Test : NSObject {
   dynamic func methodOne() -> Int { return 1 }
   dynamic func methodThree() -> String { return "three" }
+  dynamic func methodFive(string: String) -> String { return ".." + string + ".." }
+  override init(){}
 }
 
 extension Test {
-  
-  override class func initialize() {
-    /// Perform this one time only
-    struct Inner {
-      static let swizzling: () = {
-        
-        /// these 2 lines...
-        //Test.swizzle(method: #selector(Test.methodOne), with: #selector(Test.methodTwo))
-        //Test.swizzle(method: #selector(Test.methodThree), with: #selector(Test.methodFour))
-        /// are the same as:
-        Test.swizzle([
-          ( #selector(Test.methodOne), #selector(Test.methodTwo) ),
-          ( #selector(Test.methodThree), #selector(Test.methodFour) )
-          ])
-        
-      }()
-    }
-    let _ = Inner.swizzling
-  }
-  
   func methodTwo() -> Int { return methodTwo() + 10 }
-  
   func methodFour() -> String { return methodFour() + "!!" }
-  
+  func methodSix(string: String) -> String { return "--" + string + "--" }
 }
 
 class NSObjectSwizzlingTests: XCTestCase {
-  
+
+  override func setUp() {
+    Test.swizzle([
+      ( #selector(Test.methodOne), #selector(Test.methodTwo) ),
+      ( #selector(Test.methodThree), #selector(Test.methodFour) ),
+      ( #selector(Test.methodFive), #selector(Test.methodSix) )
+      ])
+  }
+
   func test_swizzlingExtension() {
     let test = Test()
     XCTAssertTrue(test.methodOne() == 11)
     XCTAssertTrue(test.methodTwo() == 1)
     XCTAssertTrue(test.methodThree() == "three!!")
     XCTAssertTrue(test.methodFour() == "three")
+    XCTAssertTrue(test.methodFive(string: "five") == "--five--")
+    XCTAssertTrue(test.methodSix(string: "six") == "..six..")
   }
-  
+
 }
