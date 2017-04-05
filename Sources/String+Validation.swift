7c7e05ea208ac199ad27a47beebb91eb74493ee5
Alessandro Marzoli <a.marzoli@ambrogio.com>
2017-04-05 16:59:52 +0200

    credits
    [ci skip]

--- Sources/String+Validation.swift
+++ Sources/String+Validation.swift
@@ -107,6 +107,7 @@ public extension String {
     //    return matches[0].url?.absoluteString == "mailto:\(self)"
 
     /// credits: https://medium.com/@darthpelo/email-validation-in-swift-3-0-acfebe4d879a
+    /// credits: http://www.cocoawithlove.com/2009/06/verifying-that-string-is-email-address.html
     let emailRegEx = "(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"+"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"+"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"+"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"+"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"+"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"+"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
     return NSPredicate(format:"SELF MATCHES[c] %@", emailRegEx).evaluate(with: self)
   }
