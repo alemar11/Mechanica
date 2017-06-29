f13e02d14165bab33410b823d0bc5c8cd3eca1fa
Alessandro Marzoli <a.marzoli@ambrogio.com>
2017-06-29 14:51:09 +0200

    --wip-- [skip ci]

--- Tests/MechanicaTests/SignedIntegerUtilsTests.swift
+++ Tests/MechanicaTests/SignedIntegerUtilsTests.swift
@@ -30,8 +30,8 @@ class SignedIntegerTests: XCTestCase {
   // MARK: - BinaryConvertible
 
   // swift 4
-//  /// http://www.binaryconvert.com/result_signed_int.html?decimal=045049049049
-//  func testBinaryString() {
+  /// http://www.binaryconvert.com/result_signed_int.html?decimal=045049049049
+  func testBinaryString() {
 //    XCTAssertEqual(Int8(10).binaryString, "00001010")
 //    XCTAssertEqual(Int8(-1).binaryString, "11111111")
 //    XCTAssertEqual(Int8(0).binaryString, "00000000")
@@ -39,25 +39,25 @@ class SignedIntegerTests: XCTestCase {
 //    XCTAssertEqual(Int8(-127).binaryString, "10000001")
 //    XCTAssertEqual(Int16(-1).binaryString, "1111111111111111")
 //    XCTAssertEqual(Int32(-111).binaryString,"11111111111111111111111110010001")
-//
-//    #if (arch(x86_64) || arch(arm64))
-//      // For 64-bit systems
-//      XCTAssertEqual((-3).binaryString, "1111111111111111111111111111111111111111111111111111111111111101")
-//      XCTAssertEqual(255.binaryString, "0000000000000000000000000000000000000000000000000000000011111111")
-//      XCTAssertEqual(1.binaryString, "0000000000000000000000000000000000000000000000000000000000000001")
-//      XCTAssertEqual(11.binaryString,"0000000000000000000000000000000000000000000000000000000000001011")
-//      XCTAssertEqual(111.binaryString,"0000000000000000000000000000000000000000000000000000000001101111")
-//      XCTAssertEqual(Int.max.binaryString,"0111111111111111111111111111111111111111111111111111111111111111")
-//    #elseif (arch(i386) || arch(arm))
-//      // For 32-bit systems
-//      XCTAssertEqual((-3).binaryString, "11111111111111111111111111111101")
-//      XCTAssertEqual(255.binaryString, "00000000000000000000000011111111")
-//      XCTAssertEqual(1.binaryString, "00000000000000000000000000000001")
-//      XCTAssertEqual(11.binaryString,"00000000000000000000000000001011")
-//      XCTAssertEqual(111.binaryString,"00000000000000000000000001101111")
-//      XCTAssertEqual(Int.max.binaryString,"01111111111111111111111111111111")
-//    #endif
-//  }
+
+    #if (arch(x86_64) || arch(arm64))
+      // For 64-bit systems
+      XCTAssertEqual((-3).binaryString, "1111111111111111111111111111111111111111111111111111111111111101")
+      XCTAssertEqual(255.binaryString, "0000000000000000000000000000000000000000000000000000000011111111")
+      XCTAssertEqual(1.binaryString, "0000000000000000000000000000000000000000000000000000000000000001")
+      XCTAssertEqual(11.binaryString,"0000000000000000000000000000000000000000000000000000000000001011")
+      XCTAssertEqual(111.binaryString,"0000000000000000000000000000000000000000000000000000000001101111")
+      //XCTAssertEqual(Int.max.binaryString,"0111111111111111111111111111111111111111111111111111111111111111")
+    #elseif (arch(i386) || arch(arm))
+      // For 32-bit systems
+      XCTAssertEqual((-3).binaryString, "11111111111111111111111111111101")
+      XCTAssertEqual(255.binaryString, "00000000000000000000000011111111")
+      XCTAssertEqual(1.binaryString, "00000000000000000000000000000001")
+      XCTAssertEqual(11.binaryString,"00000000000000000000000000001011")
+      XCTAssertEqual(111.binaryString,"00000000000000000000000001101111")
+      XCTAssertEqual(Int.max.binaryString,"01111111111111111111111111111111")
+    #endif
+  }
 
 }
 
