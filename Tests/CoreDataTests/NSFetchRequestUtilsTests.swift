d70f03497332151e71941ed42df7ced8768ca22f
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-07-12 20:37:21 +0200

    refactoring

--- Tests/CoreDataTests/NSFetchRequestUtilsTests.swift
+++ Tests/CoreDataTests/NSFetchRequestUtilsTests.swift
@@ -133,34 +133,4 @@ class NSFetchRequestUtilsTests: XCTestCase {
 
   }
 
-  //TODO move to NSPredicateUtilsTests.swift
-  func testPredicateComposition() {
-    do {
-      let predicate = NSPredicate(format: "X = 10").andPredicate(NSPredicate(format: "Y = 30"))
-      XCTAssertTrue(predicate == NSPredicate(format: "X = 10 AND Y = 30"))
-    }
-    do {
-      let predicate = NSPredicate(format: "Z = 20").orPredicate(NSPredicate(format: "K = 40"))
-      XCTAssertTrue(predicate == NSPredicate(format: "Z = 20 OR K = 40"))
-    }
-    do {
-      let predicate1 = NSPredicate(format: "X = 10").andPredicate(NSPredicate(format: "Y = 30")) // X = 10 AND Y = 30
-      let predicate2 = NSPredicate(format: "Z = 20").orPredicate(NSPredicate(format: "K = 40"))  // Z = 20 OR K = 40
-      let predicate3 = predicate1.andPredicate(predicate2)
-      XCTAssertTrue(predicate3.description == "(X == 10 AND Y == 30) AND (Z == 20 OR K == 40)")
-    }
-    do {
-      let predicate1 = NSPredicate(format: "X = 10").andPredicate(NSPredicate(format: "Y = 30")) // X = 10 AND Y = 30
-      let predicate2 = NSPredicate(format: "Z = 20").orPredicate(NSPredicate(format: "K = 40"))  // Z = 20 OR K = 40
-      let predicate3 = predicate1.orPredicate(predicate2)
-      XCTAssertTrue(predicate3.description == "(X == 10 AND Y == 30) OR (Z == 20 OR K == 40)")
-    }
-    do {
-      let predicate1 = NSPredicate(format: "X = 10 AND V = 11").andPredicate(NSPredicate(format: "Y = 30 OR W = 5"))
-      let predicate2 = NSPredicate(format: "Z = 20").orPredicate(NSPredicate(format: "K = 40 AND C = 11"))
-      let predicate3 = predicate1.orPredicate(predicate2)
-      XCTAssertTrue(predicate3.description == "((X == 10 AND V == 11) AND (Y == 30 OR W == 5)) OR (Z == 20 OR (K == 40 AND C == 11))")
-    }
-  }
-
 }
