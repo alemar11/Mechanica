2a0dd5dd33c789af8dd3e60452ac06e7f81fe295
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-09-02 19:31:32 +0200

    fixed bundleIdentifier in testsfixed bundleIdentifier in tests

--- Tests/MechanicaTests/BundleInfoTests.swift
+++ Tests/MechanicaTests/BundleInfoTests.swift
@@ -20,6 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
 import XCTest
 @testable import Mechanica
 
@@ -27,7 +28,7 @@ class BundleInfoTests: XCTestCase {
 
     func testBundle() {
       // Given, When
-      let bundle = Bundle(identifier: mechanicaBundleIdentifier)
+      let bundle = Bundle(identifier: Mechanica.bundleIdentifier)
       // Then
       XCTAssertTrue(bundle?.version != nil)
       XCTAssertTrue(bundle?.shortVersionString != nil)
