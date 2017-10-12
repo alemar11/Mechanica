1d39c681baf2edcc5f5f4dffcc3e31664faba8e5
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-10-12 21:47:42 +0200

    added a tuple result

--- Sources/RangeReplaceableCollection+Utils.swift
+++ Sources/RangeReplaceableCollection+Utils.swift
@@ -24,7 +24,7 @@
 import Foundation
 
 extension RangeReplaceableCollection where Self.Index == Int {
-
+  
   /// **Mechanica**
   ///
   /// Appends a new `element`.
@@ -32,10 +32,10 @@ extension RangeReplaceableCollection where Self.Index == Int {
   public func appending(_ element: Element) -> Self {
     var copy = self
     copy.append(element)
-
+    
     return copy
   }
-
+  
   /// **Mechanica**
   ///
   /// Removes the first element that matches the given `condition`.
@@ -44,22 +44,23 @@ extension RangeReplaceableCollection where Self.Index == Int {
   @discardableResult
   public mutating func removeFirst(where condition: (Element) -> Bool) -> Element? {
     guard let idx = index(where: condition) else { return nil }
-
+    
     return remove(at: idx)
   }
-
+  
   /// **Mechanica**
   ///
-  /// Removes the first element that matches the given `condition` and returns a `new` collection.
+  /// Removes the first element that matches the given `condition` and returns a tuple with a `new` collection and the removed element.
   /// - Parameters:
   ///   - condition: A closure that takes an element of the sequence as its argument and returns a Boolean value indicating whether the element is a match.
-  public func removingFirst(where condition: (Element) -> Bool) -> Self {
+  /// - Returns: Returns a tuple with a `new` collection and the removed element.
+  public func removingFirst(where condition: (Element) -> Bool) -> (Self, Element?) {
     var copy = self
-    copy.removeFirst(where: condition)
-
-    return copy
+    let removed = copy.removeFirst(where: condition)
+    
+    return (copy, removed)
   }
-
+  
   /// **Mechanica**
   ///
   /// Removes the last element that matches the given `condition`.
@@ -69,62 +70,65 @@ extension RangeReplaceableCollection where Self.Index == Int {
   public mutating func removeLast(where condition: (Element) -> Bool) -> Element? {
     for idx in stride(from: self.endIndex-1, through: 0, by: -1) {
       guard condition(self[idx]) else { continue }
-
+      
       return remove(at: idx)
     }
-
+    
     return nil
   }
-
+  
   /// **Mechanica**
   ///
-  /// Removes the last element that matches the given `condition` and returns a `new` collection.
+  /// Removes the last element that matches the given `condition` and returns a tuple with a `new` collection and the removed element.
   /// - Parameters:
   ///   - condition: A closure that takes an element of the sequence as its argument and returns a Boolean value indicating whether the element is a match.
-  public func removingLast(where condition: (Element) -> Bool) -> Self {
-    var items = self
-    items.removeLast(where: condition)
-
-    return items
+  /// - Returns: Returns a tuple with a `new` collection and the removed element.
+  public func removingLast(where condition: (Element) -> Bool) -> (Self, Element?) {
+    var copy = self
+    let removed = copy.removeLast(where: condition)
+    
+    return (copy, removed)
   }
-
+  
   /// **Mechanica**
   ///
   /// Removes all the elements that matches the given `condition` and returns all the removed element (if any).
   /// - Parameters:
   ///   - condition: A closure that takes an element of the sequence as its argument and returns a Boolean value indicating whether the element is a match.
+  /// - Note: use `filter` if you don't need the removed element.
   @discardableResult
   public mutating func removeAll(where condition: (Element) -> Bool) -> [Element] {
-    var removedElements: [Element]  = []
-    // fix this
-    //self = filter { !condition($0) }
+    var removed: [Element]  = []
+    
     for idx in stride(from: self.endIndex-1, through: 0, by: -1) {
       let element = self[idx]
       guard condition(element) else { continue }
-
+      
       remove(at: idx)
-      removedElements.insert(element, at: 0)
+      removed.insert(element, at: 0)
     }
-
-    return removedElements
+    
+    return removed
   }
-
+  
   /// **Mechanica**
   ///
-  /// Removes all the elements that matches the given `condition` and returns a `new` array.
+  /// Removes all the elements that matches the given `condition` and returns a tuple with a `new` collection and the removed elements.
   /// - Parameters:
   ///   - condition: A closure that takes an element of the sequence as its argument and returns a Boolean value indicating whether the element is a match.
-  public func removingAll(where condition: (Element) -> Bool) -> Self {
+  /// - Returns: Returns a tuple with a `new` collection and the removed elements.
+  /// - Note: use `filter` if you don't need the removed element.
+  public func removingAll(where condition: (Element) -> Bool) -> (Self, [Element]) {
     var copy = self
-    copy.removeAll(where: condition)
-
-    return copy
+    let removed = copy.removeAll(where: condition)
+    
+    return (copy, removed)
   }
-
+  
 }
 
 extension RangeReplaceableCollection where Self.Index == Int, Element: Equatable {
-
+  
   /// **Mechanica**
   /// Removes the first specified element from the collection (if exists).
   /// - Returns: The element removed (if any).
@@ -132,10 +136,10 @@ extension RangeReplaceableCollection where Self.Index == Int, Element: Equatable
   @discardableResult
   public mutating func removeFirstOccurrence(of element: Element) -> Element? {
     guard let idx = index(of: element) else { return nil }
-
+    
     return remove(at: idx)
   }
-
+  
   /// **Mechanica**
   ///
   /// Removes the last specified element from the array (if exists).
@@ -144,8 +148,8 @@ extension RangeReplaceableCollection where Self.Index == Int, Element: Equatable
   @discardableResult
   public mutating func removeLastOccurrence(of element: Element) -> Element? {
     if let idx = lastIndex(of: element) { return remove(at: idx) }
-
+    
     return nil
   }
-
+  
 }
