4a4790c0598170e88a81b6d3c038ad1d26267aa5
Alessandro Marzoli <a.marzoli@ambrogio.com>
2017-10-13 10:07:00 +0200

    fix

--- Sources/Dictionary+Utils.swift
+++ Sources/Dictionary+Utils.swift
@@ -121,7 +121,7 @@ extension Dictionary {
   ///
   /// Returns a random pair of (key,value) from `self`.
   public func random() -> Element {
-    let index = Int.random(min: 0, max: count-1)
+    let index = Int.random(lowerBound: 0, upperBound: count)
     let key =  Array(keys)[index]
     let value = self[key]!
 
