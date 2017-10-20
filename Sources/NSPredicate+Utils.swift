d608616dd987671a822def029ca2c3d29fc67d8e
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-10-20 19:23:52 +0200

    better file structure

--- Sources/NSPredicate+Utils.swift
+++ Sources/NSPredicate+Utils.swift
@@ -44,3 +44,37 @@ extension NSPredicate {
   }
 
 }
+
+// MARK: Operators
+
+extension NSPredicate {
+  
+  /// **Mechanica**
+  ///
+  /// Returns a `new` predicate formed by **AND-ing** the two predicates.
+  /// - Parameters:
+  ///   - lhs: The left-hand side of the operation.
+  ///   - rhs: The right-hand side of the operation.
+  public static func && (lhs: NSPredicate, rhs: NSPredicate) -> NSPredicate {
+    return NSCompoundPredicate(andPredicateWithSubpredicates: [lhs, rhs])
+  }
+  
+  /// **Mechanica**
+  ///
+  /// Returns a `new` predicate formed by **OR-ing** the two predicates.
+  /// - Parameters:
+  ///   - lhs: The left-hand side of the operation.
+  ///   - rhs: The right-hand side of the operation.
+  public static func || (lhs: NSPredicate, rhs: NSPredicate) -> NSPredicate {
+    return NSCompoundPredicate(orPredicateWithSubpredicates: [lhs, rhs])
+  }
+  
+  /// **Mechanica**
+  ///
+  /// Returns a `new` predicate forme d by **NOT-ing** a given predicate.
+  /// - Parameter p: The NSPredicate to negate.
+  public static prefix func ! (predicate: NSPredicate) -> NSPredicate {
+    return NSCompoundPredicate(notPredicateWithSubpredicate: predicate)
+  }
+  
+}
