59cf23afee5089ea6d014b2cbda5b0a3bf9773cb
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-10-30 18:27:53 +0100

    added NSImage utils (wip)

---
+++ Sources/NSImage+Utils.swift
@@ -0,0 +1,55 @@
+// 
+// Mechanica
+//
+// Copyright © 2016-2017 Tinrobots.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#if os(macOS)
+
+import AppKit
+
+extension NSImage {
+  
+//  convenience init?(base64EncodedString: String) {
+//    guard
+//      let url = URL(string: base64EncodedString),
+//      let data = try? Data(contentsOf: url)
+//      else {
+//        return nil
+//    }
+//    
+//    self.init(data: data)
+//  }
+//  
+//  class func imageNamed(name: String, inBundle bundle: Bundle?) -> NSImage? {
+//    if let image = NSImage(named: NSImage.Name(rawValue: name)) { return image }
+//    
+//    if let image = bundle?.image(forResource: NSImage.Name(rawValue: name)) {
+//      image.setName(NSImage.Name(rawValue: name))
+//      return image
+//    }
+//    
+//    return nil
+//  }
+  
+}
+
+#endif
+
