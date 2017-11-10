90c86fe03c4bbd39caa5265de3f042555626c57c
Alessandro Marzoli <a.marzoli@ambrogio.com>
2017-11-10 11:13:26 +0100

    cleaning up

--- Tests/MechanicaTests/Integer/FixedWidthIntegerRandomizableTests.swift
+++ Tests/MechanicaTests/Integer/FixedWidthIntegerRandomizableTests.swift
@@ -130,7 +130,6 @@ class FixedWidthIntegerIntervalRandomizableTests: XCTestCase {
         if (value.isNegative) { expectedAtLeastOnePositive = true }
         if (value.isPositive ){ expectedAtLeastOneNegative = true }
       }
-      //TODO fix on macOS
       XCTAssertTrue(expectedAtLeastOnePositive && expectedAtLeastOneNegative)
     }
 
