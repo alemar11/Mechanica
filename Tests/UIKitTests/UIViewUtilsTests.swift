b0f3be0f48cea71d176ae182e2f142fc94cb7f2f
Alessandro Marzoli <a.marzoli@ambrogio.com>
2017-11-14 13:10:05 +0100

    UIKit tests

---
+++ Tests/UIKitTests/UIViewUtilsTests.swift
@@ -0,0 +1,51 @@
+// 
+// Mechanica
+//
+// Copyright © 2016-2017 Tinrobots.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import XCTest
+@testable import Mechanica
+
+#if os(iOS) || os(tvOS)
+
+class UIWiewUtilsTests: XCTestCase {
+
+  func testUseAutolayout() {
+    let view = UIView()
+    view.usesAutoLayout = true
+    XCTAssertFalse(view.translatesAutoresizingMaskIntoConstraints)
+    view.usesAutoLayout = false
+    XCTAssertTrue(view.translatesAutoresizingMaskIntoConstraints)
+  }
+
+  func testScreenshot() {
+    let view = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 50)))
+    view.backgroundColor = .red
+    let screenshot = view.screenshot()
+    XCTAssertTrue(screenshot.size.width == 100)
+    XCTAssertTrue(screenshot.size.height == 50)
+    XCTAssertFalse(screenshot.size.width == 50)
+    XCTAssertFalse(screenshot.size.height == 100)
+  }
+
+}
+
+#endif
