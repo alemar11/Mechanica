0e23c1d6c68def8c122551dd6ff6f36e90b3a171
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-11-16 20:06:54 +0100

    refactor tests
    [ci skip]

--- Tests/StandardLibraryTests/FixedWidthIntegerRandomizableTests.swift
+++ Tests/StandardLibraryTests/FixedWidthIntegerRandomizableTests.swift
@@ -27,12 +27,12 @@ import XCTest
 class FixedWidthIntegerIntervalRandomizableTests: XCTestCase {
 
   static var allTests = [
-    ("testRandomlowerBound", testRandomlowerBound),
-    //("testRandomUInt", testRandomUInt),
+    ("testRandomSignedIntegerWithBounds", testRandomSignedIntegerWithBounds),
+    //("testRandomUnsignedIntegerWithBounds", testRandomUnsignedIntegerWithBounds),
     //("testOverflow", testOverflow)
   ]
 
-  func testRandomlowerBound() {
+  func testRandomSignedIntegerWithBounds() {
 
     /// Int
     XCTAssertTrue(Int.random(lowerBound: 1, upperBound: 1) == 1)
@@ -44,15 +44,14 @@ class FixedWidthIntegerIntervalRandomizableTests: XCTestCase {
     }
 
     XCTAssertTrue(Int.random(lowerBound: 1, upperBound: 100) <= 100)
-    XCTAssertTrue(Int.min...Int.max ~= Int.random())
-    XCTAssertTrue(Int32.min...Int32.max ~= Int32.random())
     XCTAssertFalse(Int.random(lowerBound: 50, upperBound: 100) > 100)
     XCTAssertFalse(Int.random(lowerBound: 40, upperBound: 50) < 40)
 
-    /// Int64
-    XCTAssertTrue(Int64.min...Int64.max ~= Int64.random())
-    XCTAssertTrue(Int64.random(lowerBound: Int64.max, upperBound: Int64.max) == Int64.max)
 
+    /// Int64
+    XCTAssertTrue(Int64.random(lowerBound: Int64.max, upperBound: Int64.max) == Int64.max)
+    XCTAssertTrue(Int64.random(lowerBound: Int64(0), upperBound: Int64(1)) == Int64(0))
+    
     /// Int32
     XCTAssertTrue(Int32.random(lowerBound: Int32.max, upperBound: Int32.max) == Int32.max)
 
@@ -66,7 +65,6 @@ class FixedWidthIntegerIntervalRandomizableTests: XCTestCase {
     }
 
     XCTAssertTrue(Int16.random(lowerBound: 1, upperBound: 100) <= 100)
-    XCTAssertTrue(Int16.min...Int16.max ~= Int16.random())
     XCTAssertFalse(Int16.random(lowerBound: 50, upperBound: 100) > 100)
     XCTAssertFalse(Int16.random(lowerBound: 40, upperBound: 50) < 40)
 
@@ -81,49 +79,75 @@ class FixedWidthIntegerIntervalRandomizableTests: XCTestCase {
     }
 
     XCTAssertTrue(Int8.random(lowerBound: 1, upperBound: 100) <= 100)
-    XCTAssertTrue(Int8.min...Int8.max ~= Int8.random())
     XCTAssertFalse(Int8.random(lowerBound: 50, upperBound: 100) > 100)
     XCTAssertFalse(Int8.random(lowerBound: 40, upperBound: 50) < 40)
 
   }
+  
+  func testSignedIntegerRandom() {
+    /// Int
+    XCTAssertTrue(Int.min...Int.max ~= Int.random())
+    
+    /// Int64
+    XCTAssertTrue(Int64.min...Int64.max ~= Int64.random())
+    
+    /// Int32
+    XCTAssertTrue(Int32.min...Int32.max ~= Int32.random())
+    
+    /// Int16
+    XCTAssertTrue(Int16.min...Int16.max ~= Int16.random())
+    
+    /// Int8
+    XCTAssertTrue(Int8.min...Int8.max ~= Int8.random())
+  }
 
-  func testRandomUInt() {
+  func testRandomUnsignedIntegerWithBounds() {
 
     /// UInt
-    XCTAssertTrue(UInt.min...UInt.max ~= UInt.random())
     XCTAssertTrue(UInt.random(lowerBound: 1, upperBound: 100) <= 100)
-    XCTAssertTrue(UInt.min...UInt.max ~= UInt.random())
     XCTAssertFalse(UInt.random(lowerBound: 50, upperBound: 100) > 100)
     XCTAssertFalse(UInt.random(lowerBound: 40, upperBound: 50) < 40)
     XCTAssertTrue(UInt.random(lowerBound: UInt.max, upperBound: UInt.max) == UInt.max)
 
     /// UInt64
     XCTAssertTrue(UInt64.random(lowerBound: UInt64.max, upperBound: UInt64.max) == UInt64.max)
-    XCTAssertTrue(UInt64.random() <= UInt64.max)
+    XCTAssertTrue(UInt64.random(lowerBound: UInt64.max-1, upperBound: UInt64.max) == UInt64.max-1)
 
     /// UInt32
-    XCTAssertTrue(UInt32.min...UInt32.max ~= UInt32.random())
     XCTAssertTrue(UInt32.random(lowerBound: 1, upperBound: 100) <= 100)
-    XCTAssertTrue(UInt32.min...UInt32.max ~= UInt32.random())
     XCTAssertFalse(UInt32.random(lowerBound: 50, upperBound: 100) > 100)
     XCTAssertFalse(UInt32.random(lowerBound: 4000, upperBound: 5000) < 4000)
     XCTAssertTrue(UInt32.random(lowerBound: UInt32.max, upperBound: UInt32.max) == UInt32.max)
 
     /// UInt16
-    XCTAssertTrue(UInt16.min...UInt16.max ~= UInt16.random())
     XCTAssertTrue(UInt16.random(lowerBound: 1, upperBound: 100) <= 100)
-    XCTAssertTrue(UInt16.min...UInt16.max ~= UInt16.random())
     XCTAssertFalse(UInt16.random(lowerBound: 50, upperBound: 100) > 100)
     XCTAssertFalse(UInt16.random(lowerBound: 4000, upperBound: 5000) < 4000)
     XCTAssertTrue(UInt16.random(lowerBound: UInt16.max, upperBound: UInt16.max) == UInt16.max)
 
     /// UInt8
     XCTAssertTrue(UInt8.random(lowerBound: 1, upperBound: 100) <= 100)
-    XCTAssertTrue(UInt8.min...UInt8.max ~= UInt8.random())
     XCTAssertFalse(UInt8.random(lowerBound: 50, upperBound: 100) > 100)
     XCTAssertFalse(UInt8.random(lowerBound: 40, upperBound: 50) < 40)
     XCTAssertTrue(UInt8.random(lowerBound: UInt8.max, upperBound: UInt8.max) == UInt8.max)
 
+  }
+  
+  func testUnsignedIntegerRandom() {
+    /// UInt
+    XCTAssertTrue(UInt.min...UInt.max ~= UInt.random())
+    
+    /// UInt64
+    XCTAssertTrue(UInt64.random() <= UInt64.max)
+    
+    /// UInt32
+    XCTAssertTrue(UInt32.min...UInt32.max ~= UInt32.random())
+    
+    /// UInt16
+    XCTAssertTrue(UInt16.min...UInt16.max ~= UInt16.random())
+    
+    /// UInt8
+    XCTAssertTrue(UInt8.min...UInt8.max ~= UInt8.random())
   }
 
   func testOverflow() {
