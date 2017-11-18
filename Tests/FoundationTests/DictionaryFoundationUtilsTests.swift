39515f3f43c8ec21d7973a57e1085525f4897ea0
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-11-18 15:18:47 +0100

    --wip-- [skip ci]

--- Tests/FoundationTests/DictionaryFoundationUtilsTests.swift
+++ Tests/FoundationTests/DictionaryFoundationUtilsTests.swift
@@ -78,7 +78,7 @@ class DictionaryFoundationUtilsTests: XCTestCase {
       let json = dictionary!.jsonString()
       XCTAssertTrue(( json == string) || (json == "{\"val\":1,\"foo\":\"bar\"}") )
       let jsonPretty = dictionary!.jsonString(prettify: true)
-      XCTAssertTrue(( jsonPretty == "{\n  \"foo\" : \"bar\",\n  \"val\" : 1\n}") || (jsonPretty == "{\n  \"val\" : 1,\n  \"foo\" : \"bar\"\n}") ) //fails on linux
+      XCTAssertTrue(( jsonPretty == "{\n  \"foo\" : \"bar\",\n  \"val\" : 1\n}") || (jsonPretty == "{\n  \"val\" : 1,\n  \"foo\" : \"bar\"\n}") || (jsonPretty == "{\n  \"val\": 1,\n  \"foo\": \"bar\"\n}")) //fails on linux
       print(jsonPretty)
     }
 
