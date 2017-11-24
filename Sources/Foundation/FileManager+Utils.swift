99297b18a2fe7c7cbbf7542977af62be8404abca
Alessandro Marzoli <a.marzoli@ambrogio.com>
2017-11-24 09:51:38 +0100

    --wip-- [skip ci]

--- Sources/Foundation/FileManager+Utils.swift
+++ Sources/Foundation/FileManager+Utils.swift
@@ -40,19 +40,18 @@ extension FileManager {
   /// - Parameter path: **directory** path (if it's not a directory path, nothing is done).
   /// - Throws:  throws an error in cases of failure.
   public final func cleanDirectory(atPath path: String) throws {
-    
-    
-    #if !os(Linux)
-      var isDirectory: ObjCBool = false
-      guard fileExists(atPath: path, isDirectory: &isDirectory) == true else { return }
-      guard isDirectory.boolValue == true else { return }
-    #else
+
+//    #if !os(Linux)
+//      var isDirectory: ObjCBool = false
+//      guard fileExists(atPath: path, isDirectory: &isDirectory) == true else { return }
+//      guard isDirectory.boolValue == true else { return }
+//    #else
       // these lines works on macOS, iOS, tvOS, watchOS
       guard fileExists(atPath: path) == true else { return }
       var res: stat = stat()
       stat(path, &res)
       guard res.isDirectory == true else { return }
-    #endif
+//    #endif
 
     let contents = try contentsOfDirectory(atPath: path)
     
@@ -103,7 +102,8 @@ fileprivate extension stat {
   }
   
   fileprivate var isDirectory: Bool {
-    return S_IFDIR == st_mode || 16877 == st_mode || 16893 == st_mode
+    //return S_IFDIR == st_mode || 16877 == st_mode || 16893 == st_mode
+    return S_ISDIR(Int(st_mode))
   }
   
   fileprivate var isFile: Bool {
@@ -111,3 +111,51 @@ fileprivate extension stat {
   }
   
 }
+
+extension stat: Equatable {
+  public static func ==(lhs: stat, rhs: stat) -> Bool {
+    return lhs.st_dev ==  rhs.st_dev && lhs.st_ino == rhs.st_ino
+  }
+
+}
+
+#if !os(Linux)
+
+public let S_IFMT : Int = 0o170000    /* [XSI] type of file mask */
+public let S_IFIFO: Int = 0o010000    /* [XSI] named pipe (fifo) */
+public let S_IFCHR: Int = 0o020000    /* [XSI] character special */
+public let S_IFDIR: Int = 0o040000    /* [XSI] directory */
+public let S_IFBLK: Int = 0o060000    /* [XSI] block special */
+public let S_IFREG: Int = 0o100000    /* [XSI] regular */
+public let S_IFLNK: Int = 0o120000    /* [XSI] symbolic link */
+public let S_IFSOCK:Int = 0o140000    /* [XSI] socket */
+
+public func S_ISBLK(_ m: Int) -> Bool {
+  return (((m) & S_IFMT) == S_IFBLK)  /* block special */
+}
+
+public func S_ISCHR(_ m: Int) -> Bool {
+  return (((m) & S_IFMT) == S_IFCHR)  /* char special */
+}
+
+public func S_ISDIR(_ m: Int) -> Bool {
+  return (((m) & S_IFMT) == S_IFDIR)  /* directory */
+}
+
+public func S_ISFIFO(_ m: Int) -> Bool {
+  return (((m) & S_IFMT) == S_IFIFO)  /* fifo or socket */
+}
+
+public func S_ISREG(_ m: Int) -> Bool {
+  return (((m) & S_IFMT) == S_IFREG)  /* regular file */
+}
+
+public func S_ISLNK(_ m: Int) -> Bool {
+  return (((m) & S_IFMT) == S_IFLNK)  /* symbolic link */
+}
+
+public func S_ISSOCK(_ m: Int) -> Bool {
+  return (((m) & S_IFMT) == S_IFSOCK)  /* socket */
+}
+
+#endif
