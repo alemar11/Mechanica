379548dab9cc8d14f90a2ef0fba7dfbd926caba1
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-11-24 19:06:04 +0100

    --wip-- [skip ci]

--- Sources/Foundation/NSAttributedString+Utils.swift
+++ Sources/Foundation/NSAttributedString+Utils.swift
@@ -25,7 +25,52 @@ import Foundation
 
 extension NSAttributedString {
 
+  #if !os(Linux)
+  // Not implemented on Linux: "mutableCopy(with:) is not yet implemented".
 
+  // MARK: - Initializers
+
+  /// **Mechanica**
+  ///
+  /// Initializes and returns a `new` NSAttributedString object from the `html` contained in the given string.
+  /// - Parameters:
+  ///   - html: an HTML string.
+  ///   - allowLossyConversion: If it is *true* and the receiver can’t be converted without losing some information, some characters may be removed or altered in conversion.
+  /// For example, in converting a character from NSUnicodeStringEncoding to NSASCIIStringEncoding, the character ‘Á’ becomes ‘A’, losing the accent.
+  /// - Note: The HTML import mechanism is meant for implementing something like markdown (that is, text styles, colors, and so on), not for general HTML import.
+  /// [Apple Documentation](https://developer.apple.com/reference/foundation/nsattributedstring/1524613-init)
+  /// - Warning: Using the HTML importer (NSHTMLTextDocumentType) is only possible on the **main thread**.
+  public convenience init?(html: String, allowLossyConversion: Bool = false) {
+    guard let data = html.data(using: .utf8, allowLossyConversion: allowLossyConversion) else { return nil }
+
+    try? self.init(data: data,
+                   options: [.documentType: NSAttributedString.DocumentType.html],
+                   documentAttributes: nil)
+  }
+
+  // MARK: - Operators
+
+  /// **Mechanica**
+  ///
+  /// Returns a `new` NSAttributedString appending the right NSAttributedString to the left NSAttributedString.
+  static public func + (lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributedString {
+    // swiftlint:disable force_cast
+    let a = lhs.mutableCopy() as! NSMutableAttributedString
+    return (a + rhs).copy() as! NSAttributedString
+    // swiftlint:enable force_cast
+  }
+
+  /// **Mechanica**
+  ///
+  /// Returns a `new` NSAttributedString appending the right String to the left NSAttributedString.
+  static func + (lhs: NSAttributedString, rhs: String) -> NSAttributedString {
+    // swiftlint:disable force_cast
+    let a = lhs.mutableCopy() as! NSMutableAttributedString
+    return (a + rhs).copy() as! NSAttributedString
+    // swiftlint:enable force_cast
+  }
+
+  #endif
 
   /// **Mechanica**
   ///
