1016e1c2c589a6659a185f815550890079addfdb
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-11-25 14:18:21 +0100

    --wip-- [skip ci]

--- Package.swift
+++ Package.swift
@@ -77,7 +77,7 @@ package.targets = [
                         "NSMutableAttributedStringUtilsTests.swift",
                         "NSObjectProtocolSwizzlingTests.swift",
 
-                        //"NSPredicateUtilsTests.swift",
+                        
                         "StringFoundationUtilsTests.swift",
                         "URLUtilsTests.swift",
                         "UserDefaultsUtilsTests.swift"
