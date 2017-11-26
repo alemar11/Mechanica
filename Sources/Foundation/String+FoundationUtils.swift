621024662fbc12a16e7903817c0179ea8c3e92e9
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-11-26 16:50:16 +0100

    --wip-- [skip ci]

--- Sources/Foundation/String+FoundationUtils.swift
+++ Sources/Foundation/String+FoundationUtils.swift
@@ -300,7 +300,7 @@ extension String {
       return NSPredicate(format: "SELF MATCHES[c] %@", emailPattern).evaluate(with: self)
     #else
       // swiftlint:disable force_cast
-      let arg: CVarArg = emailPattern as! CVarArg
+      let arg: CVarArg = NSString(string: emailPattern) as! CVarArg
       return NSPredicate(format: "SELF MATCHES[c] %@", arg).evaluate(with: self)
       // swiftlint:enable force_cast
     #endif
