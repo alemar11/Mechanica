51e590170d3d7c8867baca3d00a8984a153e6ba1
Alessandro Marzoli <a.marzoli@ambrogio.com>
2017-11-28 10:58:48 +0100

    cleaning

--- Package.swift
+++ Package.swift
@@ -5,14 +5,9 @@ import PackageDescription
 let package = Package(
   name: "Mechanica",
   products: [
-    // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    //.library(name: "Mechanica", targets: ["Mechanica"])
     .library(name: "Mechanica", targets: ["Mechanica"])
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    
     .target(name: "Mechanica", path: "Sources"),
     .testTarget(name: "FoundationTests", dependencies: ["Mechanica"]),
     .testTarget(name: "SharedTests", dependencies: ["Mechanica"]),
