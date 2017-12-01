06a5593ce08d4238d376e6c94c0a2fe8bc8cd417
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-12-01 11:47:08 +0100

    added libc target

--- Package.swift
+++ Package.swift
@@ -3,39 +3,68 @@
 import PackageDescription
 
 let package = Package(
-  name: "Mechanica",
-  products: [
-    .library(name: "Mechanica", targets: ["Mechanica"])
-  ],
-  targets: [
-    .target(name: "Mechanica", path: "Sources"),
-    .testTarget(name: "FoundationTests", dependencies: ["Mechanica"]),
-    .testTarget(name: "SharedTests", dependencies: ["Mechanica"]),
-    .testTarget(name: "StandardLibraryTests", dependencies: ["Mechanica"]),
-    .testTarget(name: "UIKitTests", dependencies: ["Mechanica"]),
-    //.testTarget(name: "AppKitTests", dependencies: ["Mechanica"]) //currently empty
-  ],
+    name: "Mechanica",
+    products: [
+        .library(name: "Mechanica", targets: ["Mechanica"])
+    ],
+    targets: [
+        .target(
+            name: "libc",
+            path: "Sources",
+            exclude: ["Info.plist"],
+            sources: ["libc"]
+        ),
+        .target(
+            name: "Mechanica",
+            dependencies: ["libc"],
+            path: "Sources",
+            sources: ["StandardLibrary", "Foundation", "Shared", "UIKit", "AppKit", "Mechanica.swift"]
+        ),
+        .testTarget(
+            name: "FoundationTests",
+            dependencies: ["Mechanica"]
+        ),
+        .testTarget(
+            name: "SharedTests",
+            dependencies: ["Mechanica"]
+        ),
+        .testTarget(
+            name: "StandardLibraryTests",
+            dependencies: ["Mechanica"]
+        ),
+        .testTarget(
+            name: "UIKitTests",
+            dependencies: ["Mechanica"]
+        ),
+        //.testTarget(name: "AppKitTests", dependencies: ["Mechanica"]) //currently empty
+    ],
     swiftLanguageVersions: [4]
 )
 
 #if os(Linux)
-  
+    
 package.targets = [
-  .target(name: "Mechanica",
-          path: "Sources",
-          exclude: ["AssociatedValueSupporting.swift", "NSObjectProtocol+Swizzling.swift"],
-          sources: ["StandardLibrary", "Foundation"]
-  ),
-  .testTarget(name: "StandardLibraryTests", dependencies: ["Mechanica"]),
-  .testTarget(name: "FoundationTests",
-              dependencies: ["Mechanica"],
-              exclude: ["AssociatedValueSupportingTests.swift",
-                        "NSAttributedStringUtilsTests.swift",
-                        "NSMutableAttributedStringUtilsTests.swift",
-                        "NSObjectProtocolSwizzlingTests.swift",
-                        "UserDefaultsUtilsTests.swift"
-    ]
-  )
+    .target(
+        name: "libc",
+        path: "Sources",
+        exclude: ["Info.plist"],
+        sources: ["libc"]
+    ),
+    .target(name: "Mechanica",
+            dependencies: ["libc"],
+            path: "Sources",
+            exclude: ["AssociatedValueSupporting.swift", "NSObjectProtocol+Swizzling.swift"],
+            sources: ["StandardLibrary", "Foundation"]
+    ),
+    .testTarget(
+        name: "StandardLibraryTests",
+        dependencies: ["Mechanica"]
+    ),
+    .testTarget(
+        name: "FoundationTests",
+        dependencies: ["Mechanica"],
+        exclude: ["AssociatedValueSupportingTests.swift", "NSAttributedStringUtilsTests.swift", "NSMutableAttributedStringUtilsTests.swift", "NSObjectProtocolSwizzlingTests.swift", "UserDefaultsUtilsTests.swift"]
+    )
 ]
-  
+    
 #endif
