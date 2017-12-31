f999490d3c1f967ca1facb32f8ee07d5d95fda57
Alessandro Marzoli <alessandromarzoli@gmail.com>
2017-12-31 18:13:18 +0100

    happy new year 🎉

--- Sources/StandardLibrary/ExpressibleByIntegerLiteral+Utils.swift
+++ Sources/StandardLibrary/ExpressibleByIntegerLiteral+Utils.swift
@@ -1,7 +1,7 @@
 //
 // Mechanica
 //
-// Copyright © 2016-2017 Tinrobots.
+// Copyright © 2016-2018 Tinrobots.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
