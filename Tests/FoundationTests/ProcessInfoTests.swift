fb7456686b6c9a5b6e4632386f1adfd404dac709
Alessandro Marzoli <alessandromarzoli@gmail.com>
2018-01-21 18:08:14 +0100

    ProcessInfo refinements

---
+++ Tests/FoundationTests/ProcessInfoTests.swift
@@ -0,0 +1,70 @@
+// 
+// Mechanica
+//
+// Copyright © 2016-2018 Tinrobots.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import XCTest
+@testable import Mechanica
+
+extension ProcessInfoTests {
+  static var allTests = [
+    ("testSystemStartingDate", testSystemStartingDate),
+    ("testIsSanboxed", testIsSanboxed),
+    ("testIsRunningUnitTests", testIsRunningUnitTests),
+    ("testIsRunningXcodeUnitTests", testIsRunningXcodeUnitTests)
+  ]
+}
+
+class ProcessInfoTests: XCTestCase {
+
+  func testSystemStartingDate() {
+    let date = ProcessInfo.systemStartingDate
+    XCTAssertTrue(date < Date())
+    XCTAssertTrue(date.timeIntervalSinceNow < 60*5)
+  }
+
+  func testIsSanboxed() {
+    #if os(macOS)
+      XCTAssertFalse(ProcessInfo.isSandboxed)
+    #endif
+  }
+
+  func testIsRunningUnitTests() {
+    XCTAssertTrue(ProcessInfo.isRunningUnitTests)
+  }
+
+  func testIsRunningXcodeUnitTests() {
+    #if os(Linux)
+      XCTAssertFalse(ProcessInfo.isRunningXcodeUnitTests)
+    #else
+      //TODO: missing test
+    #endif
+  }
+
+  func test() {
+    #if os(Linux)
+      XCTAssertTrue(ProcessInfo.isRunningSwiftPackageTests)
+    #else
+      //TODO: missing test
+    #endif
+  }
+
+}
