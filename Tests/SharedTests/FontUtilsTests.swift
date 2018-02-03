c84726fb92af3519a779adf7c5b070c1657cf67e
Alessandro Marzoli <alessandromarzoli@gmail.com>
2018-02-03 17:50:51 +0100

    added Font utils

---
+++ Tests/SharedTests/FontUtilsTests.swift
@@ -0,0 +1,69 @@
+// 
+// Mechanica
+//
+// Copyright © 2016-2018 Tinrobots.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import XCTest
+@testable import Mechanica
+
+class FontUtilsTests: XCTestCase {
+
+  func testBold() {
+    let font: Font
+    #if os(macOS)
+      font = Font.systemFont(ofSize: 16)
+    #else
+      font = Font.preferredFont(forTextStyle: .headline)
+    #endif
+
+    let boldFont = font.bold()
+    XCTAssertNotNil(boldFont)
+    XCTAssertTrue(boldFont.isBold)
+    XCTAssertFalse(boldFont.isItalic)
+  }
+
+  func testItalic() {
+    let font: Font
+    #if os(macOS)
+      font = Font.systemFont(ofSize: 16)
+    #else
+      font = Font.preferredFont(forTextStyle: .headline)
+    #endif
+
+    let italicFont = font.italic()
+    XCTAssertTrue(italicFont.isItalic)
+    XCTAssertFalse(italicFont.isBold)
+  }
+
+  func testBoldAndItalic() {
+    let font: Font
+    #if os(macOS)
+      font = Font.systemFont(ofSize: 16)
+    #else
+      font = Font.preferredFont(forTextStyle: .headline)
+    #endif
+
+    let boldAndItalicFont = font.bold().italic()
+    XCTAssertTrue(boldAndItalicFont.isItalic)
+    XCTAssertTrue(boldAndItalicFont.isBold)
+  }
+
+}
