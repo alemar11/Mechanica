3f39c5d32a7b027f72bb1143d901f5e441af7451
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-02-14 11:05:43 +0100

    fixed some warnings + migration to Xcode9.3beta

--- Sources/StandardLibrary/MutableCollection+Utils.swift
+++ Sources/StandardLibrary/MutableCollection+Utils.swift
@@ -31,8 +31,13 @@ extension MutableCollection {
     guard elementsCount > 1 else { return }
 
     for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: elementsCount, to: 1, by: -1)) {
+      #if swift(>=4.1)
+        let distance: Int = numericCast(mechanica_arc4random_uniform(numericCast(unshuffledCount)))
+      #else
       let distance: IndexDistance = numericCast(mechanica_arc4random_uniform(numericCast(unshuffledCount)))
+      #endif
       let indexToSwap = index(firstUnshuffled, offsetBy: distance)
+      
       swapAt(firstUnshuffled, indexToSwap)
     }
 
