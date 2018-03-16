d2e0f5f18c4452749a65bddc58f0a81b667d4ead
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-03-16 13:00:44 +0100

    added CGRect center

--- Sources/CoreGraphics/CGRect+Utils.swift
+++ Sources/CoreGraphics/CGRect+Utils.swift
@@ -40,6 +40,22 @@
 
     /// **Mechanica**
     ///
+    /// The center point.
+    public var center: CGPoint {
+      get {
+        return CGPoint(x: midX, y: midY)
+      }
+      set {
+        let x = newValue.x - width / 2.0
+        let y = newValue.y - height / 2.0
+        let newOrigin = CGPoint(x: x, y: y)
+
+        self.origin = newOrigin
+      }
+    }
+
+    /// **Mechanica**
+    ///
     /// Returns the `CGRect` area.
     public var area: CGFloat {
       return height * width
