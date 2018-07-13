51aeacfa5dcbc11b7020b3253a4859aa3fa4fc3c
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-07-13 14:03:04 +0200

    added lower and upper bounds

--- Sources/Foundation/Date+Utils.swift
+++ Sources/Foundation/Date+Utils.swift
@@ -560,7 +560,7 @@ public extension Date {
     guard fromDate != toDate else { return fromDate }
 
     let diff = llabs(Int64(toDate.timeIntervalSinceReferenceDate - fromDate.timeIntervalSinceReferenceDate))
-    var randomValue = Int64.random()
+    var randomValue = Int64.random(lowerBound: .min, upperBound: .max)
     randomValue = llabs(randomValue % diff)
 
     let startReferenceDate = toDate > fromDate ? fromDate : toDate
