57063958a34392830b64d6af902fffab6706974b
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-09-06 11:23:07 +0200

    testing

--- Tests/FoundationTests/NSMutableAttributedStringUtilsTests.swift
+++ Tests/FoundationTests/NSMutableAttributedStringUtilsTests.swift
@@ -282,22 +282,22 @@ final class NSMutableAttributedStringUtilsTests: XCTestCase {
         XCTAssertTrue(s.string == "Hello World")
       }
 
-//      do {
-//        let s = NSMutableAttributedString(string: "Hello", attributes: [NSAttributedStringKey("key1"): "A"])
-//        s += NSMutableAttributedString(string: " ")
-//        s += NSAttributedString(string: "World", attributes: [NSAttributedStringKey("key2"): "B"])
-//
-//        let firstCharAttributes = s.attributes(at: 0, longestEffectiveRange: nil, in: NSMakeRange(0, 0))
-//        let lastCharAttributes = s.attributes(at: 10, longestEffectiveRange: nil, in: NSMakeRange(9, 10))
-//        let a = firstCharAttributes[NSAttributedStringKey("key1")] as? String
-//        let b = lastCharAttributes[NSAttributedStringKey("key2")] as? String
-//
-//        XCTAssertNotNil(a)
-//        XCTAssertNotNil(b)
-//        XCTAssertEqual(a, "a")
-//        XCTAssertEqual(b, "b")
-//        XCTAssertTrue(s.string == "Hello World")
-//      }
+      do {
+        let s = NSMutableAttributedString(string: "Hello", attributes: [NSAttributedStringKey("key1"): "A"])
+        s += NSMutableAttributedString(string: " ")
+        s += NSAttributedString(string: "World", attributes: [NSAttributedStringKey("key2"): "B"])
+
+        let firstCharAttributes = s.attributes(at: 0, effectiveRange: nil)
+        let lastCharAttributes = s.attributes(at: 9, effectiveRange: nil)
+        let a = firstCharAttributes[NSAttributedStringKey("key1")] as? String
+        let b = lastCharAttributes[NSAttributedStringKey("key2")] as? String
+
+        XCTAssertNotNil(a)
+        XCTAssertNotNil(b)
+        XCTAssertEqual(a, "A")
+        XCTAssertEqual(b, "B")
+        XCTAssertTrue(s.string == "Hello World")
+      }
       #endif
     }
 
