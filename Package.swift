ccbb943cc10200839e10c50bfedaca0d55bd1a3d
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-01-28 22:32:25 +0100

    refactor Package

--- Package.swift
+++ Package.swift
@@ -2,22 +2,23 @@
 
 import PackageDescription
 
-let package = Package(
-  name: "Mechanica",
-  products: [
-    .library(name: "Mechanica", targets: ["Mechanica"])
-  ],
-  targets: [
-    .target(name: "Mechanica", path: "Sources"),
-    .testTarget(name: "FoundationTests", dependencies: ["Mechanica"]),
-    .testTarget(name: "SharedTests", dependencies: ["Mechanica"]),
-    .testTarget(name: "StandardLibraryTests", dependencies: ["Mechanica"]),
-    .testTarget(name: "UIKitTests", dependencies: ["Mechanica"]),
-    .testTarget(name: "AppKitTests", dependencies: ["Mechanica"]),
-    .testTarget(name: "AVFoundationTests", dependencies: ["Mechanica"])
-  ],
-  swiftLanguageVersions: [.v5]
-)
+let package = Package(name: "Mechanica")
+package.swiftLanguageVersions = [.v5]
+package.products = [.library(name: "Mechanica", targets: ["Mechanica"])]
+
+package.targets = [
+  .target(name: "Mechanica", path: "Sources"),
+  .testTarget(name: "FoundationTests", dependencies: ["Mechanica"]),
+  .testTarget(name: "SharedTests", dependencies: ["Mechanica"]),
+  .testTarget(name: "StandardLibraryTests", dependencies: ["Mechanica"]),
+  .testTarget(name: "UIKitTests", dependencies: ["Mechanica"]),
+  .testTarget(name: "AppKitTests", dependencies: ["Mechanica"]),
+  .testTarget(name: "AVFoundationTests", dependencies: ["Mechanica"])
+]
+
+//package.platforms = [
+//  .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+//]
 
 #if os(Linux)
 
