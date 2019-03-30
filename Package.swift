2a724dfa85473ddd099158e6462bdd1e2404ecb9
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-03-30 13:51:24 +0100

    update

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@
 import PackageDescription
 
 let package = Package(name: "Mechanica")
-//package.swiftLanguageVersions = [.v5]
 package.products = [.library(name: "Mechanica", targets: ["Mechanica"])]
-
 package.targets = [
   .target(name: "Mechanica", path: "Sources"),
   .testTarget(name: "FoundationTests", dependencies: ["Mechanica"]),
@@ -15,10 +13,8 @@ package.targets = [
   .testTarget(name: "AppKitTests", dependencies: ["Mechanica"]),
   .testTarget(name: "AVFoundationTests", dependencies: ["Mechanica"])
 ]
-
-//package.platforms = [
-//  .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
-//]
+package.swiftLanguageVersions = [.v5]
+package.platforms = [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)]
 
 #if os(Linux)
 
